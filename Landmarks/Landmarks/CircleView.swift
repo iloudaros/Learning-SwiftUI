14e74e1e64de8033b9802eb8281b050cadc7237c
Ioannis Loudaros <johnloudaros@hotmail.com>
2022-07-20 00:04:39 +0300

    Started "Creating and Combining Views"
    Next up: Section 5: "Use SwiftUI Views From Other Frameworks"

---
+++ Landmarks/Landmarks/CircleView.swift
@@ -0,0 +1,24 @@
+//
+//  CircleView.swift
+//  Landmarks
+//
+//  Created by Ioannis Loudaros on 19/7/22.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(.white, lineWidth: 4))
+            .shadow(radius: 7)
+        
+    }
+}
+
+struct CircleView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleView()
+    }
+}
