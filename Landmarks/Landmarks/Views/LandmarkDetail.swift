2f16a8a5ef81695b1cbc9ba5689048a1bc61d14d
Ioannis Loudaros <johnloudaros@hotmail.com>
2022-10-06 23:11:44 +0300

    Completed Building Lists and Navigation

---
+++ Landmarks/Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,50 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Ioannis Loudaros on 6/10/22.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    
+    var landmark: Landmark
+    var body: some View {
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
+                .frame(height:300)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.top/*@END_MENU_TOKEN@*/)
+            CircleImage(image: landmark.image)
+                .padding(.bottom,-130)
+                .offset(x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/-130.0/*@END_MENU_TOKEN@*/)
+                
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About \(landmark.name)").font(.title2)
+                Text(landmark.description)
+                
+            }
+            .padding()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: landmarks[0])
+    }
+}
--- Landmarks/Landmarks/Views/LandmarkList.swift
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -9,15 +9,25 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List(landmarks)
-        {
-            landmark in LandmarkRow(landmark: landmark)
+        NavigationView{
+            List(landmarks){ landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
         }
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone SE (3rd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
